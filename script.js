0931db70613756a237cc9a664858e39184c57fbc
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2024-01-09 19:00:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function dis(val) { 
+    document.getElementById("result").value += val 
+} 
+
+function myFunction(event) { 
+    if (event.key == '0' || event.key == '1' 
+        || event.key == '2' || event.key == '3' 
+        || event.key == '4' || event.key == '5' 
+        || event.key == '6' || event.key == '7' 
+        || event.key == '8' || event.key == '9' 
+        || event.key == '+' || event.key == '-' 
+        || event.key == '*' || event.key == '/') 
+        document.getElementById("result").value += event.key; 
+} 
+
+var cal = document.getElementById("calcu"); 
+cal.onkeyup = function (event) { 
+    if (event.keyCode === 13) { 
+        console.log("Enter"); 
+        let x = document.getElementById("result").value 
+        console.log(x); 
+        solve(); 
+    } 
+} 
+
+function solve() { 
+    let x = document.getElementById("result").value 
+    let y = math.evaluate(x) 
+    document.getElementById("result").value = y 
+} 
+
+function clr() { 
+    document.getElementById("result").value = "" 
+} 
